f8e36a9f7c0547f7b04f3b9e4b5a4b50e2767883
PaiCY-T <ohsendtome.jww@gmail.com>
2021-02-05 17:31:58 +0800

    calculate the average length

--- practice1.py
+++ practice1.py
@@ -1,12 +1,13 @@
 data = []
 count = 0
+total_length = 0
 with open('reviews.txt','r') as p:
 	for line in p:
 		data.append(line.strip())
 		count += 1
-		if count % 1000 == 0:
+		if count % 1000000 == 0 :
 			print(len(data))
 			count = 0
-
-
-print(len(data))
+for i in data:
+	total_length += len(i)
+print(total_length/len(data))
